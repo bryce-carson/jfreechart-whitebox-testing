4e81c76f13fca3e005e06dddf066478b434b8484
Bryce Carson <bcars268@mtroyal.ca>
2024-01-25 22:07:53 -0700

    Add everything

---
+++ test/org/jfree/data/DataUtilitiesTest.java
@@ -0,0 +1,57 @@
+package org.jfree.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DataUtilitiesTest {
+
+	// TODO
+	/* Tests I need to write for the calculateColumnTotal method
+	/* ========================================================= */
+	// returnsZeroWhenInputIsInvalid
+	@Test
+	void calculateColumnTotalReturnsZeroWhenInputIsInvalid() {
+		fail("Not yet implemented");
+	}
+	// throwsInvalidParameterExceptionWhenInvalidDataObjectPassedIn
+	@Test
+	void calculateColumnTotalThrowsInvalidParameterExceptionWhenInvalidDataObjectPassedIn() {
+		fail("Not yet implemented");
+	}
+	// returnsCorrectSum
+	@Test
+	void calculateColumnTotalReturnsCorrectSum() {
+		fail("Not yet implemented");
+	}
+	// returnsSumOfCorrectColumn
+	@Test
+	void calculateColumnTotalReturnsSumOfCorrectColumn() {
+		fail("Not yet implemented");
+	}
+	
+	// TODO
+	/* Test I need to write for the calculateRowTotal method
+	/* ===================================================== */
+	// returnsZeroWhenInputIsInvalid
+	@Test
+	void calculateRowTotalReturnsZeroWhenInputIsInvalid() {
+		fail("Not yet implemented");
+	}
+	// throwsInvalidParameterExceptionWhenInvalidDataObjectPassedIn
+	@Test
+	void calculateRowTotalThrowsInvalidParameterExceptionWhenInvalidDataObjectPassedIn() {
+		fail("Not yet implemented");
+	}
+	// returnsCorrectSum
+	@Test
+	void calculateRowTotalReturnsCorrectSum() {
+		fail("Not yet implemented");
+	}
+	// returnsSumOfCorrectRow
+	@Test
+	void calculateRowTotalReturnsSumOfCorrectRow() {
+		fail("Not yet implemented");
+	}
+
+}
