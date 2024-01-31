567cc4f702e84c2f829350004220c80109465382
Bryce Carson <bcars268@mtroyal.ca>
2024-01-31 02:26:40 -0700

    Reimplement most of the testing code I had written before
    This commit reimplements most of what I had done before I lost my work because
    of Eclipse being a difficult IDE to work with.

--- asgmt3/src/test/java/org/jfree/data/DataUtilitiesTest.java
+++ asgmt3/src/test/java/org/jfree/data/DataUtilitiesTest.java
@@ -2,7 +2,7 @@ package org.jfree.data;
 
 import static org.jfree.data.DataUtilities.calculateColumnTotal;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,15 +12,18 @@ import java.lang.reflect.Field;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.io.CSV;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class DataUtiltiesTest {
-    final public int HOUSE_PRICES_ROW_COUNT = 542;
+    final public int HOUSE_PRICES_ROW_COUNT = 546;
     public DefaultCategoryDataset housePrices, housePricesModified, housePricesEmptyTable;
 
+    /**
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
     @BeforeAll
     public void setup() throws IOException, FileNotFoundException {
         housePrices = (DefaultCategoryDataset) new CSV()
@@ -36,34 +39,49 @@ class DataUtiltiesTest {
      *
      * @param fieldNameSuffix the suffix to append to the name of the field in
      * <code>DataUtilitiesTest</code> to use for the repeated tests.
-     *
-     * FIXME: the use of reflection is unnecessary for this particular test; however, it does shorten the test and make it more readable.
      */
     @ParameterizedTest
-    @ValueSource(strings = { "", "modified", "Invalidated"})
-    public void invalidDataCausesException(String fieldNameSuffix) throws NoSuchFieldException, IllegalAccessException {
-        String fieldName = "housePrices" + fieldNameSuffix;
-        Field field = getClass().getField(fieldName);
-        DefaultCategoryDataset dataset = (DefaultCategoryDataset) field.get(field);
-
-        fail("Not yet reimplemented!");
-    }
-
-    @Test
-    public void correctSum() {
-        // The zero-indexed column to sum the values of: for this column, it
-        // happens to be the values of the row numbers, so the formula for the
-        // integer series sum 1 + 2 + 3 + ⋯ + 𝑛 is applicable here to generate
-        // the test oracle using AWK or any other language (even actual
-        // arithmetic could be used).
+    @ValueSource(strings = { "housePricesModified", "housePricesInvalidated"})
+    public void calculateColumnTotalReturnsZeroWhenPassedInvalidDataObject(String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        final Field field = getClass().getField(fieldName);
+        final DefaultCategoryDataset dataset = (DefaultCategoryDataset) field.get(field);
         final int COLUMN = 0;
-        final double COLUMN_ZERO_SUM = 123456789.0;
-        assertSame(calculateColumnTotal(housePrices, COLUMN), COLUMN_ZERO_SUM);
+        final double COLUMN_SUM = 149331.0;
+
+        assertSame(calculateColumnTotal(dataset, COLUMN), COLUMN_SUM);
     }
 
+    /**
+     * On the assumption that a NULL value in a CSV file is invalid, because the
+     * specification is ambiguous, the method should throw an {@link java.security.InvalidParameterException}.
+     *
+     * @param fieldName the suffix to append to the name of the field in
+     * <code>DataUtilitiesTest</code> to use for the repeated tests.
+     */
     @ParameterizedTest
-    @CsvFileSource(resources = "ColumnSums.csv")
-    public void somethingOrOther() {
-        fail("Not yet reimplemented!");
+    @ValueSource(strings = { "housePricesModified", "housePricesInvalidated"})
+    public void calculateColumnTotalThrowsInvalidParameterExceptionWhenPassedInvalidDataObject(String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        final Field field = getClass().getField(fieldName);
+        final DefaultCategoryDataset dataset = (DefaultCategoryDataset) field.get(field);
+        final int COLUMN = 0;
+
+        assertThrows(java.security.InvalidParameterException.class,
+                () -> calculateColumnTotal(dataset, COLUMN));
+    }
+
+    /**
+     * The method should be able to calculate the correct sum for each column.
+     * Given that the test input file has different sums for each column, any
+     * incorrect result means that the method is either calculating the correct
+     * sum of the incorrect column, or it is not calculating the correct sum for
+     * the correct column. Either possibility is a defect.
+     *
+     * @param expected The test oracle
+     * @param column The column for which the oracle value applies.
+     */
+    @ParameterizedTest
+    @CsvFileSource(resources = "HousePricesColumnSums.csv")
+    public void somethingOrOther(Double expected, int column) {
+        assertSame(calculateColumnTotal(housePrices, column), expected);
     }
 }
