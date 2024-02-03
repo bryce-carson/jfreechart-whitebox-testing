9923cab01118eebb2270fa22f1c33d339af7aeb1
Bryce Carson <bcars268@mtroyal.ca>
2024-02-02 17:32:05 -0700

    Fully implement the three tests for `calculateColumnTotal`

--- src/test/java/org/jfree/data/DataUtilitiesTest.java
+++ src/test/java/org/jfree/data/DataUtilitiesTest.java
@@ -7,18 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Field;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.io.CSV;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
-class DataUtiltiesTest {
+class DataUtilitiesTest {
     final public int HOUSE_PRICES_ROW_COUNT = 546;
-    public static DefaultCategoryDataset housePrices, housePricesModified, housePricesEmptyTable;
+    public static DefaultCategoryDataset housePrices, housePricesEmptyTable;
 
     /**
      * @throws IOException
@@ -26,62 +26,96 @@ class DataUtiltiesTest {
      */
     @BeforeAll
     public static void setup() throws IOException, FileNotFoundException {
-        housePrices = (DefaultCategoryDataset) new CSV()
-            .readCategoryDataset(new FileReader("HousePrices.csv"));
+        Path resourcesPath = Paths.get("src", "test", "resources");
+        String resourcesDirectory = resourcesPath.toFile().getAbsolutePath();
 
-        housePricesModified = (DefaultCategoryDataset) new CSV()
-            .readCategoryDataset(new FileReader("HousePricesModified.csv"));
+        String housePricesCSV = Paths.get(resourcesDirectory, "/HousePrices.csv")
+                .toAbsolutePath()
+                .toFile()
+                .getAbsolutePath();
+        housePrices = (DefaultCategoryDataset) new CSV()
+                .readCategoryDataset(new FileReader(housePricesCSV));
+
+        // Create another table like housePrices, but clear the table.
+        housePricesEmptyTable = (DefaultCategoryDataset) new CSV()
+                .readCategoryDataset(new FileReader(housePricesCSV));
+        housePricesEmptyTable.clear();
     }
 
     /**
      * On the assumption that a NULL value in a CSV file is invalid, because the
      * specification is ambiguous, the method should return zero.
-     *
-     * @param fieldNameSuffix the suffix to append to the name of the field in
-     * <code>DataUtilitiesTest</code> to use for the repeated tests.
+     * 
+     * It is only possible for the method to return zero if all of the numbers in the Values2D object are <code>NULL</code>, as a column of values whose number values are all zero is not an invalid test case, though that is the other instance in which the value returned is zero.
      */
-    @ParameterizedTest
-    @ValueSource(strings = { "housePricesModified", "housePricesInvalidated"})
-    public void zeroReturned(String fieldName) throws NoSuchFieldException, IllegalAccessException {
-        final Field field = getClass().getField(fieldName);
-        final DefaultCategoryDataset dataset = (DefaultCategoryDataset) field.get(field);
+    @Test
+    public void zeroReturned() {
         final int COLUMN = 0;
         final double COLUMN_SUM = 149331.0;
 
-        assertSame(calculateColumnTotal(dataset, COLUMN), COLUMN_SUM);
+        assertSame(COLUMN_SUM, calculateColumnTotal(housePrices, COLUMN));
     }
 
     /**
      * On the assumption that a NULL value in a CSV file is invalid, because the
-     * specification is ambiguous, the method should throw an {@link java.security.InvalidParameterException}.
-     *
-     * @param fieldName the suffix to append to the name of the field in
-     * <code>DataUtilitiesTest</code> to use for the repeated tests.
+     * specification is ambiguous, the method should throw an
+     * {@link java.security.InvalidParameterException}. The only reasonable way to
+     * produce a NULL value in the dataset is to clear the table; a NULL value in a
+     * CSV file cannot be read by JFreeChart.
+     * 
+     * TODO: learn how to generate invalid Values2D data objects.
      */
-    @ParameterizedTest
-    @ValueSource(strings = { "housePricesModified", "housePricesInvalidated"})
-    public void invalidParameterExceptionThrown(String fieldName) throws NoSuchFieldException, IllegalAccessException {
-        final Field field = getClass().getField(fieldName);
-        final DefaultCategoryDataset dataset = (DefaultCategoryDataset) field.get(field);
+    @Test
+    public void invalidParameterExceptionThrown() {
         final int COLUMN = 0;
-
         assertThrows(java.security.InvalidParameterException.class,
-                () -> calculateColumnTotal(dataset, COLUMN));
+                () -> calculateColumnTotal(housePricesEmptyTable, COLUMN));
     }
 
     /**
-     * The method should be able to calculate the correct sum for each column.
-     * Given that the test input file has different sums for each column, any
-     * incorrect result means that the method is either calculating the correct
-     * sum of the incorrect column, or it is not calculating the correct sum for
-     * the correct column. Either possibility is a defect.
-     *
-     * @param expected The test oracle
-     * @param column The column for which the oracle value applies.
-     */
+     * The method should be able to calculate the correct sum for each column. Given
+	 * that the test input file has different sums for each column, any incorrect
+	 * result means that the method is either calculating the correct sum of the
+	 * incorrect column, or it is not calculating the correct sum for the correct
+	 * column. Either possibility is a defect.
+	 * 
+	 * <pre>
+	 * 149331,0
+	 * 37194392,1
+	 * 2812045,2
+	 * 1619,3
+	 * 702,4
+	 * 987,5
+	 * 469,6
+	 * 97,7
+	 * 191,8
+	 * 25,9
+	 * 173,10
+	 * 378,11
+	 * 128,12
+	 * </pre>
+	 * 
+	 * FIXME: there are thirteen columns in the CSV <code>HousePrices.csv</code>; why is this occurring?
+	 * <pre>
+	 * <code>
+	 * java.lang.IndexOutOfBoundsException: Index 12 out of bounds for length 12
+	 *         at java.base/jdk.internal.util.Preconditions.outOfBounds(Preconditions.java:100)
+	 *         at java.base/jdk.internal.util.Preconditions.outOfBoundsCheckIndex(Preconditions.java:106)
+	 *         at java.base/jdk.internal.util.Preconditions.checkIndex(Preconditions.java:302)
+	 *         at java.base/java.util.Objects.checkIndex(Objects.java:385)
+	 *         at java.base/java.util.ArrayList.get(ArrayList.java:427)
+	 *         at org.jfree.data.DefaultKeyedValues2D.getValue(DefaultKeyedValues2D.java:136)
+	 *         at org.jfree.data.category.DefaultCategoryDataset.getValue(DefaultCategoryDataset.java:102)
+	 *         at org.jfree.data.DataUtilities.calculateColumnTotal(DataUtilities.java:69)
+	 * <code>
+	 * </pre>
+	 *
+	 * @param expected The test oracle
+	 * @param column   The column for which the oracle value applies.
+	 */
     @ParameterizedTest
-    @CsvFileSource(resources = "HousePricesColumnSums.csv")
-    public void correctColumnSums(Double expected, int column) {
-        assertSame(calculateColumnTotal(housePrices, column), expected);
+    @ArgumentsSource(CorrectColumnSumsArguments.class)
+    public void correctColumnSums(double expected, int column) {
+        assertSame(expected, calculateColumnTotal(housePrices, column));
     }
 }
